38c11ca13990d2a87f290b899b5314d2102e2a6b
Nemadede <nema6dede@gmail.com>
2022-03-20 23:24:57 +0100

    updates

--- app/controller/auth/signup.py
+++ app/controller/auth/signup.py
@@ -36,25 +36,22 @@ def verify_email(data: EmailVerificationDTO = Body(...)):
     from app.config.success_response import SuccessResponse, CustomException
     from fastapi import status
 
-    try:
-        # check if email exist
-        customer = CustomerLib.find_by(where={"email": data.email})
-        if not customer:
-            return CustomException(error="customer with this email doesn't Exist, Try signing up",
+    # check if email exist
+    customer = CustomerLib.find_by(where={"email": data.email})
+    if not customer:
+        return CustomException(error="customer with this email doesn't Exist, Try signing up",
+                               status_code=status.HTTP_400_BAD_REQUEST)
+
+    # check verification code
+    verification_info = VerificationLib.find_by(where={"code": data.code})
+    if not verification_info:
+        if customer.is_verified:
+            return CustomException(error="user is already verified",
                                    status_code=status.HTTP_400_BAD_REQUEST)
+        return CustomException(error="invalid code", status_code=status.HTTP_400_BAD_REQUEST)
 
-        # check verification code
-        verification_info = VerificationLib.find_by(where={"code": data.code})
-        if not verification_info:
-            if customer.is_verified:
-                return CustomException(error="user is already verified",
-                                       status_code=status.HTTP_400_BAD_REQUEST)
-            return CustomException(error="invalid code", status_code=status.HTTP_400_BAD_REQUEST)
+    #
+    CustomerLib.set_is_verified_true(email=customer.email)
+    VerificationLib.verify_code(verification_info=verification_info)
 
-        #
-        CustomerLib.set_is_verified_true(email=customer.email)
-        VerificationLib.verify_code(verification_info=verification_info)
-
-        return SuccessResponse(data={})
-    except Exception as e:
-        raise e
+    return SuccessResponse(data={})
