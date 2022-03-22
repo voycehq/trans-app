22f9bf9160a92f4f2311bcc3c2d81613428c2300
Nemadede <nema6dede@gmail.com>
2022-03-22 14:30:12 +0100

    updated dtos

--- app/dto/model/translated_text.py
+++ app/dto/model/translated_text.py
@@ -11,8 +11,8 @@ class TranslatedTextDTO(BaseModel):
     body: Text
     reviewed_by: Optional[int]
     reviewed_date: Optional[datetime]
-    translated_date: datetime
-    audio_generation_date: datetime
+    translated_date: Optional[datetime]
+    audio_generation_date: Optional[datetime]
     created_on: datetime
     updated_on: Optional[datetime]
     deleted_on: Optional[datetime]
