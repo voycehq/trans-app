57cfc533e18dea29827a8f14b37b612fc743aec1
real-music <fedjioraymond@gmail.com>
2022-03-23 20:26:13 +0100

    updates

--- frontend/api/client.tsx
+++ frontend/api/client.tsx
@@ -1,7 +1,8 @@
 import { create } from "apisauce";
 import authStorage from "../store";
+import { http } from "../libs/utils";
 
-const client = create({ baseURL: "http://localhost:4000" });
+const client = create({ baseURL: http });
 client.addAsyncRequestTransform(async (request) => {
   const apiKey = authStorage.getState().getApiKey();
   if (!apiKey) return;
